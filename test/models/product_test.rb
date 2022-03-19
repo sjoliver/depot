66835680b7b6d013d5102501ac8fa7eee2313295
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-03-18 18:43:17 -0700

    add unit tests for image_url validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,4 +9,44 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+
+  test "product price must be positive" do 
+    product = Product.new(title: "My Book Title",
+                          description: "yyy",
+                          image_url:"zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "My Book Title",
+                description: "yyy",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+            "#{image_url} must be valid"
+    end
+    
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+            "#{image_url} must be invalid"
+    end
+  end
 end
