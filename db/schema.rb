3de6549447e6ac843178561238147b3d295b8e49
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-03-23 19:14:24 -0700

    add new cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_17_204617) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_015424) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
