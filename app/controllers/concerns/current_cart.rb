1948f59581fd03c878cac1b0edaa9addc22d35bc
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-03-23 19:40:11 -0700

    new module containing method for finding the session cart or creating a new cart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,6 +1,9 @@
 module CurrentCart
+  # private prevents Rails from making the set_cart method available as an action on the controller
   private
 
+  # looks to find a cart given a cart_id in the session
+  # if no cart is found, an exception is raised and a new cart is created 
   def set_cart
     @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
