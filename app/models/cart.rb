3340d9bf4aa2479ec1c077c0e685b0f28bba058f
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-03-23 20:03:34 -0700

    new line item scaffold, add relationship between line items, carts, and products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
 class Cart < ApplicationRecord
+  # if we destroy a cart, all line items with that cart_id should be destroyed too
+  has_many :line_items, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_015424) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_024113) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -25,4 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_015424) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
