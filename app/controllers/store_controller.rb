ea87a6b4b5c21e1d69037b9a3db2c5723224de6d
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-03-25 12:09:55 -0700

    add new session counter to record how many times a user accesses the store index page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+
+    puts "INDEX COUNTS", session[:counter]
+
   end
 end
